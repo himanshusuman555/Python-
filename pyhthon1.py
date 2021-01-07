bb9cd0441b5540ff02cc2135a459af8dce414bdb
himanshusuman555 <46639298+himanshusuman555@users.noreply.github.com>
2021-01-07 20:38:41 +0530

    Add files via upload

---
+++ pyhthon1.py
@@ -0,0 +1,74 @@
+Python 3.9.1 (tags/v3.9.1:1e5d33e, Dec  7 2020, 17:08:21) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Rakesh kumar")
+Rakesh kumar
+>>> print('Rakesh kumar')
+Rakesh kumar
+>>> print('rakesh'+'kumar')
+rakeshkumar
+>>> print('rakesh' 'kumar')
+rakeshkumar
+>>> print(""" rakesh kumar""")
+ rakesh kumar
+>>> print('''rakesh kumar''')
+rakesh kumar
+>>> 2+4
+6
+>>> 3-1
+2
+>>> 3/2
+1.5
+>>> 3//2
+1
+>>> 7//3
+2
+>>> 7/3
+2.3333333333333335
+>>> 5%8
+5
+>>> 
+>>> 5 % 8
+5
+>>> 7//3
+2
+>>> 6>5
+True
+>>> 6<5
+False
+>>> a=10
+>>> b=5
+>>> ar=a*b
+>>> ar
+50
+>>> print("area of rectangle= "+ar)
+Traceback (most recent call last):
+  File "<pyshell#22>", line 1, in <module>
+    print("area of rectangle= "+ar)
+TypeError: can only concatenate str (not "int") to str
+>>> print("area of rectangle = "+ar)
+Traceback (most recent call last):
+  File "<pyshell#23>", line 1, in <module>
+    print("area of rectangle = "+ar)
+TypeError: can only concatenate str (not "int") to str
+>>>  print("area of rectangle = "+ar)
+ 
+SyntaxError: unexpected indent
+>>> print("area of rectangle="+ar)
+Traceback (most recent call last):
+  File "<pyshell#25>", line 1, in <module>
+    print("area of rectangle="+ar)
+TypeError: can only concatenate str (not "int") to str
+>>> print("Area =",ar)
+Area = 50
+>>> 
+===== RESTART: C:/Users/Hp/AppData/Local/Programs/Python/Python39/first.py =====
+Before Swapping
+20      3
+After swapping
+3       20
+>>> 
+===== RESTART: C:/Users/Hp/AppData/Local/Programs/Python/Python39/second.py ====
+Before Swapping 20 3
+After Swapping
+3      20
+>>> 

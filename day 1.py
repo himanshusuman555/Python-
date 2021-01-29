ba4c10be132904e934fa81c3096275fe39697235
himanshusuman555 <himanshusuman555@gmail.com>
2021-01-29 21:36:45 +0530

    Add files via upload

---
+++ day 1.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello world")
+
+
+# In[2]:
+
+
+#This is a comment
+print("Hello, world!")
+
+
+# In[3]:
+
+
+"""This is a 
+multiline docstring."""
+print("Hello, world!")
+
+
+# In[4]:
+
+
+x = 5
+y= "john"
+print(x)
+print(y)
+
+
+# In[5]:
+
+
+x ="awesome"
+print("python is "+x )
+
+
+# In[7]:
+
+
+x ="python is"
+y =" awesome"
+z=x + y
+print(z)
+
+
+# In[8]:
+
+
+x =1
+y=2.8
+z= 1j
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+# In[9]:
+
+
+x =1
+y =35656222554887711
+z = -3255522
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+# In[10]:
+
+
+x =1.10
+y= 1.0
+z= -35.59
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+# In[ ]:
+
+
+
+
+
+# In[12]:
+
+
+x= 35e3
+y =12E4
+z =-87.7e100
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+# In[13]:
+
+
+x=3+5j
+y=5j
+z=-5j
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+# In[ ]:
+
+
+
+
